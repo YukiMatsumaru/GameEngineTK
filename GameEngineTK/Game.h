68c66ca90a4693b52a75ec47a9bbb1b04cecfed9
Matsumaru <s162166>
2017-04-20 10:40:58 +0900

    三角形・3D化・デバックカメラの追加

--- GameEngineTK/Game.h
+++ GameEngineTK/Game.h
@@ -5,7 +5,13 @@
 #pragma once
 
 #include "StepTimer.h"
+#include "DebugCamera.h"
 
+#include <VertexTypes.h>
+#include <PrimitiveBatch.h>
+#include <Effects.h>
+#include <CommonStates.h>
+#include <SimpleMath.h>
 
 // A basic game implementation that creates a D3D11 device and
 // provides a game loop.
@@ -62,4 +68,16 @@ private:
 
     // Rendering loop timer.
     DX::StepTimer                                   m_timer;
+
+	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionNormal>>m_batch;
+	std::unique_ptr<DirectX::BasicEffect> m_effect;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
+
+	DirectX::SimpleMath::Matrix m_world;
+	DirectX::SimpleMath::Matrix m_view;
+	DirectX::SimpleMath::Matrix m_proj;
+
+	//デバックカメラ
+	std::unique_ptr<DebugCamera>m_debugCamera;
+
 };
